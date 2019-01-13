52289ebf26d0ff3f07808e5710f6cd3714de599d
Pieter Vogelaar <pieter@pietervogelaar.nl>
2019-01-13 13:47:35 +0100

    Make Dockerfile.dev consistent with Dockerfile

--- .container/start.sh
+++ .container/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Make sure the kubeconfig file exists to avoid errors with kubectl
-mkdir /etc/.kube
-touch /etc/.kube/config
-
 # Start supervisor
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
